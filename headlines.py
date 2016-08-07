ebacab9f14b0f560e6594e36dab7bc0b8dc3a098
Sachin <sachin.nicky@gmail.com>
2016-08-07 23:28:09 +0530

    Refactoring to include weather info

--- headlines.py
+++ headlines.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
+import json
+import urllib
 
 import feedparser
 
 app =Flask(__name__)
+
+DEFAULTS = {
+    "publication" : "bbc",
+    "city" : "London, UK"
+}
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=49edf448fe3e52ba925bd5549dc6ffc6"
+
 
 RSS_FEEDS = {
     "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
@@ -13,16 +23,40 @@ RSS_FEEDS = {
 
 }
 
-
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
+def home():
+    publication = request.args.get("publication")
+    if not publication:
+        publication = DEFAULTS["publication"]
+    articles = get_news(publication)
+    city = request.args.get("city")
+    if not city:
+        city = DEFAULTS["city"]
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather, publication=RSS_FEEDS)
+
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed["entries"], publication=RSS_FEEDS)
+    return feed["entries"]
+
+def get_weather(query):
+    query = urllib.parse.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib.request.urlopen(url).read().decode("utf-8")
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
+                  }
+    return weather
+
+
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
