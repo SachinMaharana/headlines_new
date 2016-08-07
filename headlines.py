38257f87c60506634b54afa0e0815d0c95d7e242
Sachin <sachin.nicky@gmail.com>
2016-08-07 13:46:45 +0530

    Http get using form

--- headlines.py
+++ headlines.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template
-import feedparser
+from flask import Flask, render_template, request
 
+import feedparser
 
 app =Flask(__name__)
 
 RSS_FEEDS = {
     "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
-    "hollywood": "http://feeds.feedburner.com/thr/news",
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
-    "screen": "http://screenrant.com/feed/",
-    "slash" :"http://feeds2.feedburner.com/slashfilm",
-    "time": "http://time.com/feed/"
+    "time": "http://time.com/feed/",
+    "ndtv": "http://feeds.feedburner.com/ndtvnews-latest",
+    "for" : "http://fortune.com/feed/"
+
 }
 
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    # first_article = feed["entries"][0]
     return render_template("home.html", articles=feed["entries"])
 
 if __name__ == "__main__":
