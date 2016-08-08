c871f60e97c09f088f87d98fb94ac7e508c0aab1
Sachin <sachin.nicky@gmail.com>
2016-08-08 21:55:04 +0530

    Added Currency option

--- headlines.py
+++ headlines.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import datetime
+from flask import make_response
 import json
 import urllib
 
@@ -8,9 +10,12 @@ app =Flask(__name__)
 
 DEFAULTS = {
     "publication" : "bbc",
-    "city" : "London, UK"
+    "city" : "London, UK",
+    "currency_from" : "GBP",
+    "currency_to" : "USD"
 }
 
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=2504504501d94da3aa68e00eec522526"
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=49edf448fe3e52ba925bd5549dc6ffc6"
 
 
@@ -33,7 +38,14 @@ def home():
     if not city:
         city = DEFAULTS["city"]
     weather = get_weather(city)
-    return render_template("home.html", articles=articles, weather=weather, publication=RSS_FEEDS)
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS["currency_from"]
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS["currency_to"]
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles=articles, weather=weather,currency_from=currency_from, currency_to=currency_to,rate=rate, currencies=sorted(currencies), publication=RSS_FEEDS)
 
 
 def get_news(query):
@@ -55,7 +67,12 @@ def get_weather(query):
                   }
     return weather
 
-
+def get_rate(frm, to):
+    all_currency = urllib.request.urlopen(CURRENCY_URL).read().decode("utf-8")
+    parsed = json.loads(all_currency).get("rates")
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate / frm_rate, parsed.keys())
 
 
 if __name__ == "__main__":
